81a75e572d2d7fe280a6663e5fc0f2b78a08381f
carinezoe <zoe.dreessen@student.pxl.be>
2025-12-17 19:39:44 +0100

    WEB-08: Changed workExperience from hover to click

--- js/script.js
+++ js/script.js
@@ -1,25 +1,22 @@
-function fullWidth (number) {
-    const element = document.getElementById('workExperience_' + number);
-    const mateNumber = getRowMate(number);
-    const rowElement = document.getElementById('workExperience_' + mateNumber);
-
-    element.classList.add('fullWidth');
-    rowElement.classList.add('hidden');
-}
-
-function normalWidth (number) {
-    const element = document.getElementById('workExperience_' + number);
-    const mateNumber = getRowMate(number);
-    const rowElement = document.getElementById('workExperience_' + mateNumber);
-
-    element.classList.remove('fullWidth');
-    rowElement.classList.remove('hidden');
-}
-
 function getRowMate(number) {
     if (number % 2 !== 0) {
         return number + 1;
     } else {
         return number - 1;
     }
+}
+
+function widthCheck(number) {
+    const element = document.getElementById('workExperience_' + number);
+    const taskPackage = element.querySelector('p[class^="taskPackage"]');
+    const mateNumber = getRowMate(number);
+    const rowElement = document.getElementById('workExperience_' + mateNumber);
+
+    const isOpen = element.classList.contains('fullWidth');
+
+    element.classList.toggle('fullWidth', !isOpen);
+    rowElement.classList.toggle('hidden', !isOpen);
+
+    taskPackage.classList.toggle('taskPackageHide', isOpen);
+    taskPackage.classList.toggle('taskPackageShow', !isOpen);
 }
